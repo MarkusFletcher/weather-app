6efcd9a4d42a9f27eb91b028eea0b17d1c5d19d0
MarkusFletcher <cehatopfatalist@gmail.com>
2023-07-01 23:42:44 +0300

    change template

--- src/components/weather/current/Current.tsx
+++ src/components/weather/current/Current.tsx
@@ -1,5 +1,6 @@
 import { IWeatherCurrentData } from '../../../types/weather.types'
 import s from './Current.module.scss'
+import { TiLocation } from 'react-icons/ti'
 
 interface Props {
   weather: IWeatherCurrentData
@@ -7,7 +8,7 @@ interface Props {
 }
 
 export const Current: React.FC<Props> = ({ weather, openSearch }) => {
-  const time = weather.current.last_updated.split(' ')[1]
+  const [date, time] = weather.current.last_updated.split(' ')
   const dayOrNightImgUrl = `status/${
     weather.current.is_day ? 'sun' : 'moon'
   }.svg`
@@ -18,10 +19,21 @@ export const Current: React.FC<Props> = ({ weather, openSearch }) => {
         <img src={dayOrNightImgUrl} alt=''></img>
       </div>
       <div className={s.info}>
-        <div className={s.time}>{time}</div>
-        <div className={s.temperature}>{weather.current.temp_c}°C</div>
-        <div className={s.location} onClick={openSearch}>
-          {`${weather.location.name}, ${weather.location.country}`}
+        <div className={s.field}>
+          <div className={s.time}>{time}</div>
+        </div>
+        <div className={s.field}>
+          <img src={weather.current.condition.icon} alt='' />
+          <div className={s.temperature}>{weather.current.temp_c}°C</div>
+        </div>
+        <div className={s.field}>
+          <TiLocation fontSize={20}></TiLocation>
+          <div className={s.location} onClick={openSearch}>
+            {`${weather.location.name}, ${weather.location.country}`}
+          </div>
+        </div>
+        <div className={s.field}>
+          <div className={s.condition}>{weather.current.condition.text}</div>
         </div>
       </div>
     </div>
