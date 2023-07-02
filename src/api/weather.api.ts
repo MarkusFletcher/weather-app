1d17187f36cf8313bf4df4f1726ef6b4d3a624de
MarkusFletcher <cehatopfatalist@gmail.com>
2023-07-02 20:11:35 +0300

    fix

--- src/api/weather.api.ts
+++ src/api/weather.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const weatherApi = axios.create({
-  baseURL: 'http://api.weatherapi.com/v1',
+  baseURL: 'https://api.weatherapi.com/v1',
   params: {
     key: '33f4d472f10e4b3eb15151411232106',
     lang: 'ru',
