0ea37c27ffed139c10344267b7e7d1b043e4f1b5
MarkusFletcher <cehatopfatalist@gmail.com>
2023-07-02 17:31:54 +0300

    fix

--- src/api/weather.api.ts
+++ src/api/weather.api.ts
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
-const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL
-
 export const weatherApi = axios.create({
-  baseURL: WEATHER_API_URL,
+  baseURL: process.env.REACT_APP_WEATHER_API_KEY,
   params: {
-    key: WEATHER_API_KEY,
+    key: process.env.REACT_APP_WEATHER_API_URL,
     lang: 'ru',
   },
 })
